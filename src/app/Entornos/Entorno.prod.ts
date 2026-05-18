c3bc3efe655790696eb467ad62045c88c87c2266
prosystem-pro <prosystem490@gmail.com>
2026-05-17 20:28:33 -0600

    Despliegue inicial

---
+++ src/app/Entornos/Entorno.prod.ts
@@ -0,0 +1,5 @@
+export const Entorno = {
+  Produccion: true,
+  ApiUrl: 'https://sastreria-demo-api-production.up.railway.app/api/',
+  NombreEmpresa: 'Demo'
+};
---
+++ src/app/Entornos/Entorno.ts
@@ -0,0 +1,6 @@
+export const Entorno = {
+    Produccion: false,
+    ApiUrl: 'http://localhost:3000/api/' ,
+    NombreEmpresa: 'Demo'
+  };
+  
