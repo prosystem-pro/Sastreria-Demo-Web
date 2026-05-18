8a0bc601a2fbf4d06b26c5a94e5f56418cf06018
prosystem-pro <prosystem490@gmail.com>
2026-05-17 23:48:11 -0600

    klk

--- src/app/Paginas/Inicio/menu/menu.component.ts
+++ src/app/Paginas/Inicio/menu/menu.component.ts
@@ -13,13 +13,13 @@ export class MenuComponent {
   CentrarVertical = true;
   opciones = [
     { nombre: 'Clientes', icono: 'bi-people-fill', ruta: '/cliente' },
-    // { nombre: 'Pedidos', icono: 'bi-bag-fill', ruta: '/pedido-listado' },
-    // { nombre: 'Historial de Pedidos', icono: 'bi-clock-history', ruta: '/pedido-historial-listado' },
+    { nombre: 'Pedidos', icono: 'bi-bag-fill', ruta: '/pedido-listado' },
+    { nombre: 'Historial de Pedidos', icono: 'bi-clock-history', ruta: '/pedido-historial-listado' },
     { nombre: 'Inventario', icono: 'bi-box-seam', ruta: '/inventario-listado' },
     { nombre: 'Ventas', icono: 'bi-cash-stack', ruta: '/venta-listado' },
-    // { nombre: 'Anexos', icono: 'bi-paperclip', ruta: '/menu-oficial' },
-    { nombre: 'Reportes', icono: 'bi-bar-chart-fill', ruta: '/reporte-venta' }
-    // { nombre: 'Configuración', icono: 'bi-gear-fill', ruta: '/configuracion-listado' }
+    { nombre: 'Anexos', icono: 'bi-paperclip', ruta: '/menu-oficial' },
+    { nombre: 'Reportes', icono: 'bi-bar-chart-fill', ruta: '/reporte-venta' },
+    { nombre: 'Configuración', icono: 'bi-gear-fill', ruta: '/configuracion-listado' }
   ];
 
 
